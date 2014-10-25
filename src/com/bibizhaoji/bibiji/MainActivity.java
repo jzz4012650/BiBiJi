65183a1b04ada64994ac35552103477737e020cd
yy4020 <402019887@qq.com>
2014-10-25 18:12:12 +0800

    Revert "test a"
    This reverts commit 87d2dab44a862f3dd9dd3d5d5e9b7afefb61c4cf.

--- src/com/bibizhaoji/bibiji/MainActivity.java
+++ src/com/bibizhaoji/bibiji/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-// a
 public class MainActivity extends Activity implements
 	NavigationDrawerFragment.NavigationDrawerCallbacks {
 
